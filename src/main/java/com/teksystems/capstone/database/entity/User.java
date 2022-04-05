49a2fe38dd1fd03fd640f38de8e677e607864d49
LittleMagician <bthaoxaochay@gmail.com>
2022-04-04 21:32:19 -0500

    Fixed db issues in order to build app

--- src/main/java/com/teksystems/capstone/database/entity/User.java
+++ src/main/java/com/teksystems/capstone/database/entity/User.java
@@ -3,6 +3,7 @@ package com.teksystems.capstone.database.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -13,5 +14,6 @@ import javax.persistence.Table;
 @Table(name = "users")
 public class User {
     @Id
+    @Column(name = "id")
     private int userId;
 }
