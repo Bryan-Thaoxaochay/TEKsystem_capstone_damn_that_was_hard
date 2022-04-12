1d4d802b24eb65f43019315b31b656087359ddb0
LittleMagician <bthaoxaochay@gmail.com>
2022-04-11 21:52:02 -0500

    Password is hashed in db when a user signs up

--- src/main/java/com/teksystems/capstone/controller/UserController.java
+++ src/main/java/com/teksystems/capstone/controller/UserController.java
@@ -4,6 +4,7 @@ import com.teksystems.capstone.bean.SignUpBean;
 import com.teksystems.capstone.database.dao.UserDAO;
 import com.teksystems.capstone.database.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,9 @@ import org.springframework.web.servlet.ModelAndView;
 public class UserController {
     @Autowired
     private UserDAO userDAO;
+
+    @Autowired
+    private PasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping("/user/sign_up")
     public ModelAndView displaySignUpPage() throws Exception {
@@ -39,7 +43,7 @@ public class UserController {
         user.setLastName(form.getLastName());
         user.setUsername(form.getUsername());
         user.setEmail(form.getEmail());
-        user.setPassword(form.getPassword());
+        user.setPassword(bCryptPasswordEncoder.encode(form.getPassword()));
 
         userDAO.save(user);
 
