10856a55bc203fc364f4991ff86fcff59b32db73
LittleMagician <bthaoxaochay@gmail.com>
2022-04-20 11:21:21 -0500

    Created and finished CRUD tests for CommentDAOTest.java

--- src/test/java/com/teksystems/capstone/database/dao/CommentDAOTest.java
+++ src/test/java/com/teksystems/capstone/database/dao/CommentDAOTest.java
@@ -69,4 +69,51 @@ public class CommentDAOTest {
         log.info("----------------------" + commentDAO.findById(1).getComment());
         Assertions.assertEquals("0: This story was amazing!", commentDAO.findById(1).getComment());
     }
+
+    // CRUD Tests
+    @Test
+    @Order(3)
+    public void createComment() {
+        User user = userDAO.findByEmail("john_doe@example.com");
+        List<Post> post = postDAO.findByUserId(user.getUserId());
+
+        Comment newComment = new Comment();
+        newComment.setComment("Holy Crap!");
+        newComment.setBlogpostId(post.get(0).getUserId());
+        newComment.setUserId(user.getUserId());
+        commentDAO.save(newComment);
+
+        List<Comment> comments = commentDAO.findAll();
+        Assertions.assertEquals(commentDAO.findById(6), comments.get(comments.size() - 1));
+    }
+
+    @Test
+    @Order(4)
+    public void readComment() {
+        Comment comment = commentDAO.findById(1);
+        log.info("------------Comment: " + comment.getComment());
+        Assertions.assertEquals("0: This story was amazing!", comment.getComment());
+    }
+
+    @Test
+    @Order(5)
+    public void updateComment() {
+        String newComment = "Wowwwwwww";
+        Comment comment = commentDAO.findById(1);
+        comment.setComment(newComment);
+        commentDAO.save(comment);
+
+        Comment updatedComment = commentDAO.findById(1);
+        log.info("---------------------New Comment: " + updatedComment.getComment());
+        Assertions.assertEquals(newComment, updatedComment.getComment());
+    }
+
+    @Test
+    @Order(6)
+    public void deleteComment() {
+        Comment comment = commentDAO.findById(1);
+        commentDAO.delete(comment);
+
+        Assertions.assertNull(commentDAO.findById(1));
+    }
 }
