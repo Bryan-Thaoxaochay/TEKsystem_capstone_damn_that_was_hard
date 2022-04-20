7cbca718ec67b87208c938f1e5e0dfba0ab25be4
LittleMagician <bthaoxaochay@gmail.com>
2022-04-20 14:17:16 -0500

    Changed findByUsername query into a non-native hibernate query w/ @Query

--- src/main/java/com/teksystems/capstone/database/dao/UserDAO.java
+++ src/main/java/com/teksystems/capstone/database/dao/UserDAO.java
@@ -2,12 +2,14 @@ package com.teksystems.capstone.database.dao;
 
 import com.teksystems.capstone.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDAO extends JpaRepository<User, Long> {
-    User findByUsername(@Param("username") String username);
+    @Query(value = "select u from User u where u.username = ?1")
+        User findByUsername(String username);
     User findByEmail(@Param("email") String email);
     User findByUserId(@Param("userId") Integer userId);
 }
